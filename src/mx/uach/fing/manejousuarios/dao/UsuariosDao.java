ee59d731f0a438597d9f43cf78827aecf56b0759
SaariiR <saarii.rodriguez@gmail.com>
2015-08-19 09:26:13 -0600

    PrimeraPractica

---
+++ src/mx/uach/fing/manejousuarios/dao/UsuariosDao.java
@@ -0,0 +1,32 @@
+package mx.uach.fing.manejousuarios.dao;
+
+import java.util.List;
+import mx.uach.fing.manejousuarios.datos.Usuario;
+
+/**
+ *
+ * @author Sara Rodriguez Soto
+ */
+public class UsuariosDao implements Crud {
+
+    @Override
+    public List<Usuario> list() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void create(Usuario usuario) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(Usuario usuario) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+}
